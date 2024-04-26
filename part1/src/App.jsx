65ad9d44c020ec5a9e0a36899dcc872b48af0f1a
Mario <mario@colmancol.com>
2024-04-26 15:41:43 -0500

    exercise 1.7

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -4,17 +4,24 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [click, setClick] = useState(0)
+  const [average, setAverage] = useState(0)
 
   const handleGood = () => {
     setGood(good + 1)
+    setClick(click + 1)
+    setAverage(average + 1)
   }
 
   const handleNeutral = () => {
     setNeutral(neutral + 1)
+    setClick(click + 1)
   }
 
   const handleBad = () => {
     setBad(bad + 1)
+    setClick(click + 1)
+    setAverage(average - 1)
   }
 
   return (
@@ -38,6 +45,9 @@ const App = () => {
         <p>good {good}</p>
         <p>neutral {neutral}</p>
         <p>bad {bad}</p>
+        <p>all {click}</p>
+        <p>average {average/click}</p>
+        <p>positive {(good/click)*100}%</p>
       </div>
     </div>
   )
