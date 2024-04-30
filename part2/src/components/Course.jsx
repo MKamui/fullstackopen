0bb3fdae9493a5a33be3ec52b3eb882cbc0ebb84
Mario <mario@colmancol.com>
2024-04-30 15:31:20 -0500

    exercise 2.2 and 2.3

--- part2/src/components/Course.jsx
+++ part2/src/components/Course.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 
 const Course = ({course}) => {
+    const totalEx = course.parts.reduce((sum, part) => sum + part.exercises, 0)
+
   return (
     <div>
         <h1>{course.name}</h1>
-        <div>{course.parts.map(part => 
+        <div>
+            {course.parts.map(part => 
             <p key={part.id}>
                 {part.name} {part.exercises}
             </p>)}
+            <h3>Total of {totalEx} exercises</h3>
         </div>
     </div>
   )
